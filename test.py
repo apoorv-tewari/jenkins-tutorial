fe9c3cbaf517220836627bad4021e35e0653fdb3
apoorv-tewari <thisisapoorve@gmail.com>
2024-08-27 21:54:38 +0530

    sample python programme for testing build in jenkins

---
+++ test.py
@@ -0,0 +1,13 @@
+# Sample Python program for Jenkins build
+
+def main():
+    # Print a greeting message
+    print("Hello, Apoorve!")
+    
+    # Print a sample build message
+    print("This is a sample python programme build for Jenkins.")
+
+if __name__ == "__main__":
+    main()
+
+
